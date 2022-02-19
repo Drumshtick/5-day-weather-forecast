91f09b6fef35f0356c461e2f3bf58888310f5b1d
Drumshtick <ekodnar@outlook.com>
2022-02-18 20:40:43 -0600

    finished layout with hardocded values

--- src/components/dayListItem/DayListItem.jsx
+++ src/components/dayListItem/DayListItem.jsx
@@ -1,7 +1,21 @@
 import styles from './DayListItem.module.scss';
-
+import Image from 'next/image';
 export default function DayListItem() {
   return (
-    <li></li>
+    <li className={styles.container}>
+      <header className={styles.day}>Mon</header>
+      <div className={styles.imgWrapper}>
+        <Image
+          height={55}
+          width={55}
+          alt="sunny day"
+          src="https://openweathermap.org/img/wn/02d.png"
+        />
+      </div>
+      <footer className={styles.temp}>
+        <div className={styles.dayTemp}>27&#176;</div>
+        <div className={styles.nightTemp}>18&#176;</div>
+      </footer>
+    </li>
   );
 };
