e71a30e22052a16d8501d36d7cd8cc47a9367419
Drumshtick <ekodnar@outlook.com>
2022-02-18 18:22:51 -0600

    setup directory structure

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.scss';
+
+export default function Home() {
+  
+  return (
+    <div>
+      <Head>
+        <title>5 Day Weather Forecaster</title>
+        <meta name="description" content="See the weather for the next few" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <main className={styles.mainContainer}></main>
+    </div>
+  )
+}
