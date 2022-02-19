f33147a81ec04a48cede4a82b290039fe9f00d7e
Drumshtick <ekodnar@outlook.com>
2022-02-19 15:43:14 -0600

    imported useLocationData

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,18 @@
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.scss';
 import DayList from '../components/dayList/DayList'
+import useLocationData from '../hooks/useLocationData';
 
 export default function Home() {
+  const { latitude } = useLocationData();
+
   
+
   return (
     <div>
       <Head>
         <title>5 Day Weather Forecaster</title>
-        <meta name="description" content="See the weather for the next few" />
-        <link rel="icon" href="/favicon.ico" />
-        <link href="http://fonts.cdnfonts.com/css/poppins" rel="stylesheet" />
       </Head>
     <main className={styles.mainContainer}>
       <DayList />
