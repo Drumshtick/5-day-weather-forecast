a346fcc63486c1e3207924f8d43fe8a756c7d81f
Drumshtick <ekodnar@outlook.com>
2022-02-19 19:11:35 -0600

    implemented cache

--- src/pages/api/get4DayForecast.js
+++ src/pages/api/get4DayForecast.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+const NodeCache = require( "node-cache" );
+const cache = new NodeCache( { stdTTL: 1800 } );
 
 const get4DayForecast = async (req, res) => {
   if (!req.query) {
@@ -6,21 +8,29 @@ const get4DayForecast = async (req, res) => {
     return res.status(400).json({ 'error': 'missing coords'});
   }
   const { lat, long } = req.query;
-  const openWeatherURL = `
-  ${process.env.OPEN_WEATHER_API_BASE}lat=${lat}&lon=${long}&appid=${process.env.OPEN_WEATHER_API_KEY}
-  `;
-  console.log(openWeatherURL)
-  axios.get(openWeatherURL)
-  .then(response => {
-    console.log(response.data)
-    return res.status(200).json(response.data);
-  })
-  .catch(err => {
-    console.log("Error in getting openWeather data(4Day): ", err.message);
-    return res.status(400).json({ 'error': err.message});
-  });
+  const coordKey = `${lat},${long}`;
 
-
-}
+  const weatherData = cache.get(coordKey);
+  if ( !weatherData ) {
+    console.log("Making call to openWeather....")
+    const openWeatherURL = `
+    ${process.env.OPEN_WEATHER_API_BASE}lat=${lat}&lon=${long}&appid=${process.env.OPEN_WEATHER_API_KEY}
+    `;
+    axios.get(openWeatherURL)
+    .then(response => {
+      let success = cache.set(coordKey, response.data);
+      if ( success ) console.log("cache set too: ", coordKey)
+      return res.status(200).json(response.data);
+    })
+    .catch(err => {
+      console.log("Error in getting openWeather data(4Day): ", err.message);
+      return res.status(400).json({ 'error': err.message});
+    });
+  }
+  if ( weatherData ) {
+    console.log("Found data in cache for: ", coordKey)
+    return res.status(200).json(weatherData);
+  }
+};
 
 export default get4DayForecast;
