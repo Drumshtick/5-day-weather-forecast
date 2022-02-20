c6023aadd0dc2c96f02b9b22903702cff90a4420
Drumshtick <ekodnar@outlook.com>
2022-02-19 18:55:27 -0600

    wrote test request to openweather

--- src/pages/api/get4DayForecast.js
+++ src/pages/api/get4DayForecast.js
@@ -1,5 +1,26 @@
 import axios from 'axios';
 
-export default function get4DayForecast(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const get4DayForecast = async (req, res) => {
+  if (!req.query) {
+    console.log('Request to get4Day failed: missing params')
+    return res.status(400).json({ 'error': 'missing coords'});
+  }
+  const { lat, long } = req.query;
+  const openWeatherURL = `
+  ${process.env.OPEN_WEATHER_API_BASE}lat=${lat}&lon=${long}&appid=${process.env.OPEN_WEATHER_API_KEY}
+  `;
+  console.log(openWeatherURL)
+  axios.get(openWeatherURL)
+  .then(response => {
+    console.log(response.data)
+    return res.status(200).json(response.data);
+  })
+  .catch(err => {
+    console.log("Error in getting openWeather data(4Day): ", err.message);
+    return res.status(400).json({ 'error': err.message});
+  });
+
+
 }
+
+export default get4DayForecast;
