8308c5821754ae6cef4d087e3e5506a4ff751765
Drumshtick <ekodnar@outlook.com>
2022-02-20 19:15:32 -0600

    passed relevant props to dayListItem

--- src/components/dayList/DayList.jsx
+++ src/components/dayList/DayList.jsx
@@ -1,14 +1,37 @@
 import styles from './DayList.module.scss'
 import DayListItem from '../dayListItem/DayListItem';
+import getDayOfWeek from '../helpers/getWeekAbbreviation';
 
-export default function DayList() {
+export default function DayList(props) {
+  const { forecast, loading } = props;
+  let forecastDay;
+  
+  if (!loading) {
+    const forecastKeys = Object.keys(forecast)
+    forecastDay = forecastKeys.map(( day, i ) => {
+
+      const date = new Date(0);
+      date.setSeconds(forecast[day].dt);
+      const dayName = getDayOfWeek(date.getDay());
+
+      return (
+        <DayListItem
+          key={i}
+          day={dayName}
+          imageIcon={forecast[day].weather[0].icon}
+          forecastData={forecast[day]}
+          current={i === 0 ? true : false}
+        />
+      );
+    })
+  }
+
+  
   return (
     <ul className={styles.container}>
-      <DayListItem />
-      <DayListItem />
-      <DayListItem />
-      <DayListItem />
-      <DayListItem />
+      {!loading && forecastDay}
     </ul>
   );
 };
+
+
