cf42b203192221cebf2629e1def7387643ad6a19
Drumshtick <ekodnar@outlook.com>
2022-02-20 19:13:59 -0600

    created helper to convert int day of week to abrev

---
+++ src/components/helpers/getWeekAbbreviation.js
@@ -0,0 +1,13 @@
+export default function getDayOfWeek(day) {
+  const days = {
+    0: 'Sun',
+    1: 'Mon',
+    2: 'Tue',
+    3: 'Wed',
+    4: 'Thu',
+    5: 'Fri',
+    6: 'Sat'
+  };
+
+  return days[day] || null;
+};
