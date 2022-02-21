c56f8eb01923c423e7298c6bdf55814967e5c098
Drumshtick <ekodnar@outlook.com>
2022-02-20 19:48:40 -0600

    started implementing forecast data

--- src/components/dayListItem/DayListItem.jsx
+++ src/components/dayListItem/DayListItem.jsx
@@ -2,22 +2,27 @@ import styles from './DayListItem.module.scss';
 import Image from 'next/image';
 
 export default function DayListItem(props) {
-  const { current } = props;
   console.log(props)
+  const {
+    current,
+    day,
+    imageIcon,
+    forecastData
+  } = props;
   return (
     <li className={`${styles.container} ${current && styles.current}`}>
-      <header className={styles.day}>Mon</header>
+      <header className={styles.day}>{day}</header>
       <div className={styles.imgWrapper}>
         <Image
           height={55}
           width={55}
           alt="sunny day"
-          src="https://openweathermap.org/img/wn/02d.png"
+          src={`https://openweathermap.org/img/wn/${imageIcon}.png`}
         />
       </div>
       <footer className={styles.temp}>
-        <div className={styles.dayTemp}>27&#176;</div>
-        <div className={styles.nightTemp}>18&#176;</div>
+        <div className={styles.dayTemp}>{Number(forecastData.temp.day) - Number(process.env.NEXT_PUBLIC_K_VALUE)}&#176;</div>
+        <div className={styles.nightTemp}>{forecastData.temp.night}&#176;</div>
       </footer>
     </li>
   );
